bf67d417edffe0fb5644a5efa940ca7a025cd288
seka1 <2714877+seka19@users.noreply.github.com>
2020-11-03 04:27:05 +0100

    fix: fix missed "use"

--- src/ShopifyApp/Middleware/AuthProxy.php
+++ src/ShopifyApp/Middleware/AuthProxy.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Config;
 use OhMyBrew\ShopifyApp\Facades\ShopifyApp;
 use OhMyBrew\ShopifyApp\Services\JwtService;
 
